18e4f63568b469d32673b66661fc6312c41d8a12
hpeyerl <hpeyerl@beer.org>
2015-09-07 07:17:38 -0600

    Erase flash first; then run minicom last.

--- firmware/burn.sh
+++ firmware/burn.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 erase_flash
 sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x00000 firmware/0x00000.bin
 sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x40000 firmware/0x40000.bin
 sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x12000 webpages.espfs
 sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x3C000 blank.bin
-
+sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 run && minicom ttyUSB0
