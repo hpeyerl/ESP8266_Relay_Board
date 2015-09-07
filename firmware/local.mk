b9b2775315385aa13c19a0f60799d9f192115fc9
hpeyerl <hpeyerl@beer.org>
2015-09-07 06:47:37 -0600

    switch to "C" esptool.

--- firmware/local.mk
+++ firmware/local.mk
@@ -2,4 +2,4 @@ XTENSA_TOOLS_ROOT ?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/xtensa-lx106-el
 SDK_EXTRA_INCLUDES ?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/esp_iot_sdk_v1.2.0/sdk/include /home/hpeyerl/trunk/toolchains/esp-open-sdk/esp_iot_sdk_v1.2.0/examples/driver_lib/include 
 SDK_EXTRA_LIBS ?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/crosstool-NG/.build/xtensa-lx106-elf/buildtools/bin/xtensa-lx106-elf-gcc
 SDK_BASE	?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/esp_iot_sdk_v1.2.0
-ESPTOOL		?= esptool.py
+ESPTOOL		?= /home/hpeyerl/trunk/toolchains/esptool/esptool
