2fafdcf0b7bd542428fa7afb0512aa7bf9d4f3e6
hpeyerl <hpeyerl@beer.org>
2015-09-07 06:40:25 -0600

    Reduce changeset to Makefile;  use local.mk instead.

---
+++ firmware/local.mk
@@ -0,0 +1,5 @@
+XTENSA_TOOLS_ROOT ?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/xtensa-lx106-elf/bin/
+SDK_EXTRA_INCLUDES ?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/esp_iot_sdk_v1.2.0/sdk/include /home/hpeyerl/trunk/toolchains/esp-open-sdk/esp_iot_sdk_v1.2.0/examples/driver_lib/include 
+SDK_EXTRA_LIBS ?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/crosstool-NG/.build/xtensa-lx106-elf/buildtools/bin/xtensa-lx106-elf-gcc
+SDK_BASE	?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/esp_iot_sdk_v1.2.0
+ESPTOOL		?= esptool.py
