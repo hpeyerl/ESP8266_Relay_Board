d8e784c0b1d32a56e35c7554bef831e053c5b542
hpeyerl <hpeyerl@beer.org>
2015-09-09 06:11:45 -0600

    Localize. Don't push.

--- firmware/burn.sh
+++ firmware/burn.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 erase_flash
-sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x00000 firmware/0x00000.bin
-sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x40000 firmware/0x40000.bin
-sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x12000 webpages.espfs
-sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x3C000 blank.bin
-sudo ./esptool.py --port /dev/ttyUSB0 --baud 115200 run && minicom ttyUSB0
+./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x00000 firmware/0x00000.bin
+./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x40000 firmware/0x40000.bin
+./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x12000 webpages.espfs
+./esptool.py --port /dev/ttyUSB0 --baud 115200 write_flash 0x3C000 blank.bin
+./esptool.py --port /dev/ttyUSB0 --baud 115200 run && minicom ttyUSB0
