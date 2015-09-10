5cc44948c288722d076b43132eedc1d8f42c1318
hpeyerl <hpeyerl@beer.org>
2015-09-10 05:57:40 -0600

    I don't need sudo.

--- firmware/burn3.sh
+++ firmware/burn3.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-sudo ./esptool.py --port /dev/ttyUSB0 write_flash 0x12000 webpages.espfs
+./esptool.py --port /dev/ttyUSB0 write_flash 0x12000 webpages.espfs
+./esptool.py --port /dev/ttyUSB0 --baud 115200 run && minicom ttyUSB0
 
