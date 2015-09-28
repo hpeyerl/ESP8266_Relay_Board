99608e83dd9335f5a361493d92f78d166e305fe5
hpeyerl <hpeyerl@beer.org>
2015-09-28 07:00:15 -0600

    Some debug stuff. So we can read the E^2 calibration settings as well as the USER1 register and firmware rev.

--- firmware/driver/i2c_si7020.c
+++ firmware/driver/i2c_si7020.c
@@ -13,11 +13,36 @@
 
 static bool IS_ALREADY_INITED = false;
 
+static uint8_t ICACHE_FLASH_ATTR
+SI7020_Read_EE(uint8_t addr)
+{
+	uint8_t x;
+
+	i2c_master_start();
+	i2c_master_writeByte(SI7020_ADDRESS);
+	if (!i2c_master_checkAck()) os_printf("Missed ACK 0\n");
+	i2c_master_writeByte(SI7020_READ_EE);
+	if (!i2c_master_checkAck()) os_printf("Missed ACK 1\n");
+	i2c_master_writeByte(addr);
+	if (!i2c_master_checkAck()) os_printf("Missed ACK 2\n");
+	i2c_master_start();
+	i2c_master_writeByte(SI7020_ADDRESS|1);	// read 
+	if (!i2c_master_checkAck()) os_printf("Missed ACK 3\n");
+	x = i2c_master_readByte();
+	i2c_master_send_nack();
+	i2c_master_stop();
+	return x;
+}
+
 bool ICACHE_FLASH_ATTR
 SI7020_Init()
 {
+#ifdef CONFIG_CMD_SI7020_DEBUG
 	uint8_t datum;
-	uint32_t *serial_num;
+	uint16_t x;
+	uint8_t offset;
+	uint8_t len;
+#endif
 	i2c_master_gpio_init(5, 2);	// hardcoded ickiness!
 
 	if (i2c_master_writeBytes1(SI7020_ADDRESS, SI7020_RESET))
@@ -25,20 +50,28 @@ SI7020_Init()
 		os_delay_us(15000);	// max time after issuing reset
 		IS_ALREADY_INITED = true;
 #ifdef CONFIG_CMD_SI7020_DEBUG
+		datum = 0;
 		i2c_master_writeBytes1(SI7020_ADDRESS, SI7020_USER1);
 		i2c_master_readUint8(SI7020_ADDRESS, 0, &datum);
 		os_printf("%s: User 1 says: 0x%x %s\n", __FUNCTION__, datum, (datum&4?"Heater is on!":""));
 		i2c_master_writeBytes1(SI7020_ADDRESS, SI7020_USER1);
-		datum &= ~4;	// turn off heater just in case it's on
-		i2c_master_writeBytes1(SI7020_ADDRESS, datum);
 #endif
-		i2c_master_writeBytes1(SI7020_ADDRESS, SI7020_SERIAL_A);
-		i2c_master_readBytes(SI7020_ADDRESS, (uint8_t *)&serial_num, 4);
-		os_printf("%s: Serial A is: %x", __FUNCTION__, serial_num);
-		i2c_master_writeBytes1(SI7020_ADDRESS, SI7020_SERIAL_B);
-		i2c_master_readBytes(SI7020_ADDRESS, (uint8_t *)&serial_num, 4);
-		os_printf("%s: Serial B is: %x", __FUNCTION__, serial_num);
+#ifdef CONFIG_CMD_SI7020_DEBUG
+			os_printf("\tFWREV: 0x%02x\n", SI7020_Read_EE(0xb8));
+#endif
 
+#ifdef CONFIG_CMD_SI7020_DEBUG
+		offset = 0xe;
+		len = 14;
+		for (x=0; x<len; x++) {
+			os_printf("\t0x%02x: 0x%02x\n", offset+x, SI7020_Read_EE(offset+x));
+		}
+
+		offset = 0x6e;
+		os_printf("\n\t0x%02x: 0x%02x\n", offset, SI7020_Read_EE(offset));
+		offset++;
+		os_printf("\t0x%02x: 0x%02x\n", offset, SI7020_Read_EE(offset));
+#endif
 		return true;
 	}
 
@@ -99,7 +132,7 @@ SI7020_GetTemperature()
 		 * CPU. This affects the humidity reading also.  So artifically adjust the temp
 		 * here and humidity in SI7020_GetHumidity.
 		 */
-		temp = ((((datum[1]<<8)|datum[0])*175.72)/65536)-51.85;	// should be -46.85
+		temp = ((((datum[1]<<8)|datum[0])*175.72)/65536)-46.85;	// should be -46.85
 	else
 		temp = 0xffff;
 	os_printf("%s: datum is: 0x%x 0x%x  temp is 0x%x\n", __FUNCTION__, datum[1], datum[0], temp);
@@ -125,7 +158,7 @@ SI7020_GetHumidity()
 	 * See note in SI7020_GetTemperature
 	 */
 	if((SI7020_Read_Humidity((uint16_t *)datum)))
-		hum = ((((datum[1]<<8)|datum[0])*125.0)/65536)-4.0; // Should be -6.0
+		hum = ((((datum[1]<<8)|datum[0])*125.0)/65536)-6.0; // Should be -6.0
 	else
 		hum = 0xffff;
 	return hum;
