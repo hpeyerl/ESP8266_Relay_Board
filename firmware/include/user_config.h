59d7b30c4ba6c1013448450474669a1500100d8a
Herb Peyerl <hpeyerl+gitlab@beer.org>
2015-10-31 07:10:46 -0600

    stop trying to connect to dd-wrt.

--- firmware/include/user_config.h
+++ firmware/include/user_config.h
@@ -17,8 +17,8 @@
 #define STA_IP       "192.168.1.17"
 #define STA_MASK     "255.255.255.0"
 #define STA_GW       "192.168.1.1"
-#define STA_SSID     "dd-wrt" 
-#define STA_PASS     "a1b2c3d4e5"
+#define STA_SSID     "" 
+#define STA_PASS     ""
 #define STA_TYPE AUTH_WPA2_PSK
 
 #define AP_IP        "192.168.4.1"
