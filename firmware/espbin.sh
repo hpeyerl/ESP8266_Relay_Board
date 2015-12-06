6f3beae871d78bffe326e125deedb9e91cb79719
Herb Peyerl <hpeyerl+gitlab@beer.org>
2015-12-06 05:13:08 -0700

    a couple helpful scripts.

---
+++ firmware/espbin.sh
@@ -0,0 +1,3 @@
+esptool -v -eo build/httpd.out -bo firmware/0x00000.bin -bs .text -bs .data -bs .rodata -bc -ec -eo  build/httpd.out -es .irom0.text firmware/0x40000.bin -ec
+esptool -v -cp /dev/ttyUSB0 -cd none -cb 115200 -ca 0x00000 -cf firmware/0x00000.bin -ca 0x40000 -cf firmware/0x40000.bin
+minicom ttyUSB0
