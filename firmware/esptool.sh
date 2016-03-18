e892d00ac9d599c787e77f914e49e0a5ce27c0fc
Herb Peyerl <hpeyerl+gitlab@beer.org>
2016-03-18 07:24:34 -0600

    fixup like esptool4.sh

--- firmware/esptool.sh
+++ firmware/esptool.sh
@@ -1,3 +1,4 @@
-esptool -v -eo build/httpd.out -bo firmware/0x00000.bin -bs .text -bs .data -bs .rodata -bc -ec -eo  build/httpd.out -es .irom0.text firmware/0x40000.bin -ec
-esptool -v -cp /dev/ttyUSB0 -cd none -cb 115200 -ca 0x00000 -cf firmware/0x00000.bin -ca 0x40000 -cf firmware/0x40000.bin -ca 0x12000 -cf webpages.espfs
+ESPTOOL=../../../tools/esptool-ck/esptool
+$ESPTOOL -v -eo build/httpd.out -bo firmware/0x00000.bin -bs .text -bs .data -bs .rodata -bc -ec -eo  build/httpd.out -es .irom0.text firmware/0x40000.bin -ec
+$ESPTOOL -v -cp /dev/ttyUSB0 -cd none -cb 115200 -ca 0x00000 -cf firmware/0x00000.bin -ca 0x40000 -cf firmware/0x40000.bin -ca 0x12000 -cf webpages.espfs
 minicom ttyUSB0
