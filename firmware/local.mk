3d11ffad6d873dbf5d6f8c3c7384084ca7c0be46
Herb Peyerl <hpeyerl+gitlab@beer.org>
2016-03-25 06:15:17 -0600

    switch away from SVN tree to my GIT tree.

--- firmware/local.mk
+++ firmware/local.mk
@@ -1,6 +1,6 @@
-XTENSA_TOOLS_ROOT ?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/xtensa-lx106-elf/bin/
-SDK_EXTRA_INCLUDES ?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/esp_iot_sdk_v1.2.0/sdk/include /home/hpeyerl/trunk/toolchains/esp-open-sdk/esp_iot_sdk_v1.2.0/examples/driver_lib/include 
-SDK_EXTRA_LIBS ?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/crosstool-NG/.build/xtensa-lx106-elf/buildtools/bin/xtensa-lx106-elf-gcc
-SDK_BASE	?= /home/hpeyerl/trunk/toolchains/esp-open-sdk/esp_iot_sdk_v1.2.0
+XTENSA_TOOLS_ROOT ?= /home/hpeyerl/phrobs/tools/xtensa-lx106-elf/bin/
+SDK_BASE	?= /home/hpeyerl/phrobs/tools/esp_toolchain/esp_iot_sdk_v1.4.0
+SDK_EXTRA_INCLUDES ?= $(SDK_BASE)/sdk/include $(SDK_BASE)/examples/driver_lib/include 
+SDK_EXTRA_LIBS ?= $(SDK_BASE)/crosstool-NG/.build/xtensa-lx106-elf/buildtools/bin/xtensa-lx106-elf-gcc
 #ESPTOOL		?= /home/hpeyerl/trunk/toolchains/esptool/esptool
 ESPTOOL		?= /home/hpeyerl/phrobs/tools/esptool-ck/esptool
