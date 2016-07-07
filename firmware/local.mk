71d739f57fa1182bf925e51ddf17a55cadda87e0
Herb Peyerl <hpeyerl+gitlab@beer.org>
2016-07-07 06:54:39 -0600

    updated paths.  remove absolute paths and use whatever project.mk gives us.

--- firmware/local.mk
+++ firmware/local.mk
@@ -1,7 +1,6 @@
-SDK_VERSION ?= 140
-XTENSA_TOOLS_ROOT ?= /home/hpeyerl/phrobs/tools/xtensa-lx106-elf/bin/
-SDK_BASE	?= /home/hpeyerl/phrobs/tools/esp_toolchain/esp_iot_sdk_v1.4.0
+-include ../../../esp-project.mk
+SDK_VERSION ?= 154
+XTENSA_TOOLS_ROOT ?= $(TOOLCHAIN_DIR)
+SDK_BASE	?= $(TOOLS_DIR)/ESP8266_NONOS_SDK_V1.5.4_16_05_20
 SDK_EXTRA_INCLUDES ?= $(SDK_BASE)/sdk/include $(SDK_BASE)/examples/driver_lib/include 
 SDK_EXTRA_LIBS ?= $(SDK_BASE)/crosstool-NG/.build/xtensa-lx106-elf/buildtools/bin/xtensa-lx106-elf-gcc
-#ESPTOOL		?= /home/hpeyerl/trunk/toolchains/esptool/esptool
-ESPTOOL		?= /home/hpeyerl/phrobs/tools/esptool-ck/esptool
