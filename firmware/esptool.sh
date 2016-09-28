b0366d9b8ae69213e83904950f3b5da2bb4de9ea
Herb Peyerl <hpeyerl+gitlab@beer.org>
2016-09-28 05:50:12 -0600

    be smarter about finding esptool

--- firmware/esptool.sh
+++ firmware/esptool.sh
@@ -1,5 +1,4 @@
-ESPTOOL=`grep ^ESPTOOL local.mk | awk '{print $NF}'`
-echo $ESPTOOL
+ESPTOOL=`make show.ESPTOOL | cut -d= -f 2`
 $ESPTOOL -v -eo build/httpd.out -bo firmware/0x00000.bin -bs .text -bs .data -bs .rodata -bc -ec -eo  build/httpd.out -es .irom0.text firmware/0x40000.bin -ec
 $ESPTOOL -v -cp /dev/ttyUSB0 -cd none -cb 230400 -ca 0x00000 -cf firmware/0x00000.bin -ca 0x40000 -cf firmware/0x40000.bin -ca 0x12000 -cf webpages.espfs
 minicom ttyUSB0
--- firmware/esptool4.sh
+++ firmware/esptool4.sh
@@ -1,4 +1,4 @@
-ESPTOOL=../../../tools/esptool-ck/esptool
+ESPTOOL=`make show.ESPTOOL | cut -d= -f 2`
 $ESPTOOL -v -bz 4M -bm dio -eo build/httpd.out -bo firmware/0x00000.bin -bs .text -bs .data -bs .rodata -bc -ec -eo  build/httpd.out -es .irom0.text firmware/0x40000.bin -ec
 $ESPTOOL -v -cp /dev/ttyUSB0 -cd none -cb 115200 -ca 0x00000 -cf firmware/0x00000.bin -ca 0x40000 -cf firmware/0x40000.bin -ca 0x12000 -cf webpages.espfs
 minicom ttyUSB0
